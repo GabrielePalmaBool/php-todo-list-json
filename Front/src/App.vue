7e48f0b1304363e306820dbb787fe2532a26ad15
Gabriele <gabriele.palma.bool@gmail.com>
2024-01-25 14:52:51 +0100

    first commit

---
+++ Front/src/App.vue
@@ -0,0 +1,48 @@
+<script>
+//importo libreria axios
+import axios from "axios";
+
+//importo store
+import { store } from "./store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  methods: {
+    getCards() {
+      let myURL = store.apiURL;
+
+      //Se è stato selezionato qualcosa dal selettore
+      if (store.searchText !== "") {
+        myURL = `https://db.ygoprodeck.com/api/v7/cardinfo.php?${store.Arc}=${store.searchText}`;
+      }
+
+      console.log(myURL);
+      //prima chiamata axios
+      axios
+        .get(myURL)
+        .then((res) => {
+          store.TodoList = res.data.data;
+          console.log(store.CardList);
+        })
+        .catch((err) => {
+          console.log("Errori", err);
+        });
+    },
+  },
+
+  created() {
+    this.getCards();
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
